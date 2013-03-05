713218502789a592fa213cfb043ec1ce9b6c9a1d
Tobias Kraze <tobias@kraze.eu>
2013-03-05 12:34:56 +0100

    get rid of unnecessary database cleaner

--- spec/shared/support/database_cleaner.rb
+++
@@ -1,6 +0,0 @@
-DatabaseCleaner.strategy = :truncation
-
-(defined?(Spec) ? Spec::Runner : RSpec).configure do |config|
-  config.before(:each) { DatabaseCleaner.start }
-  config.after(:each) { DatabaseCleaner.clean }
-end
