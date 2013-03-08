79b416c1bc1ed3ae4a567936ff7ac245fee9e4bd
Tobias Kraze <tobias@kraze.eu>
2013-03-08 19:56:46 +0100

    fix capistrano tasks

--- lib/secret_service/capistrano.rb
+++ lib/secret_service/capistrano.rb
@@ -1,27 +1,29 @@
-namespace :secret_service do
-
-  def _run_with_prompt(command)
-    run command do |input, stream, out|
-      puts out
-      if stream == :out and out =~ /^Enter/
-        input.send_data STDIN.gets
-      end
+def run_with_prompt(command)
+  run command do |input, stream, out|
+    puts out
+    if stream == :out and out =~ /^Enter/
+      input.send_data STDIN.gets
     end
   end
+end
 
-  desc "Store a secret using secret_service"
-  task :store, :roles => :db, :only => { :primary => true } do
-    rake = fetch(:rake, "rake")
-    rails_env = fetch(:rails_env, "production")
+Capistrano::Configuration.instance(:must_exist).load do
+  namespace :secret_service do
 
-    _run_with_prompt("cd #{current_path} && #{rake} RAILS_ENV=#{rails_env} secret_service:store")
-  end
+    desc "Store a secret using secret_service"
+    task :store, :roles => :db, :only => { :primary => true } do
+      rake = fetch(:rake, "rake")
+      rails_env = fetch(:rails_env, "production")
 
-  desc "Show a secret previously stored with secret_service"
-  task :show, :roles => :db, :only => { :primary => true } do
-    rake = fetch(:rake, "rake")
-    rails_env = fetch(:rails_env, "production")
+      run_with_prompt("cd #{current_path} && #{rake} RAILS_ENV=#{rails_env} secret_service:store")
+    end
 
-    _run_with_prompt("cd #{current_path} && #{rake} RAILS_ENV=#{rails_env} secret_service:show")
+    desc "Show a secret previously stored with secret_service"
+    task :show, :roles => :db, :only => { :primary => true } do
+      rake = fetch(:rake, "rake")
+      rails_env = fetch(:rails_env, "production")
+
+      run_with_prompt("cd #{current_path} && #{rake} RAILS_ENV=#{rails_env} secret_service:show")
+    end
   end
 end
